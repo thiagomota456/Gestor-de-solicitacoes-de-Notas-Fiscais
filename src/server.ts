ce631e88616c843bd8ac6b6e80aab7062c8ef3ab
Thiago Soares Mota <thiagomota456@gmail.com>
2025-11-30 14:46:31 -0300

    Add logs

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,24 @@ import express from 'express';
 import { PrismaClient } from '@prisma/client';
 import 'dotenv/config';
 import { router } from './routes';
+import { pinoHttp } from 'pino-http';
+import { logger } from './utils/logger';
 
 const app = express();
 const prisma = new PrismaClient();
+
+app.use(pinoHttp({
+    logger,
+    autoLogging: {
+        ignore: (req) => req.url === '/health',
+    },
+    serializers: {
+        req: (req) => ({
+            method: req.method,
+            url: req.url,
+        }),
+    }
+}));
 
 app.use(express.json());
 app.use(router);
